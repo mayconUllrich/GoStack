ec730fdd71e6834783326077d20cf30c0df70330
mayconullrich <maycon123@gmail.com>
2020-04-07 20:53:40 -0300

    Primeiro commit.
     No ramo master
    
     Submissão inicial.
    
     Mudanças a serem submetidas:
    	new file:   aulas/conceitos-dev/backend/.gitignore
    	new file:   aulas/conceitos-dev/backend/package.json
    	new file:   aulas/conceitos-dev/backend/src/index.js
    	new file:   aulas/conceitos-dev/backend/yarn.lock

---
+++ aulas/conceitos-dev/backend/src/index.js
@@ -0,0 +1,13 @@
+ const express = require('express')
+
+ const app = express()
+
+app.get('/projects', (request, response) => {
+  return response.json({
+    message: 'Hello World'
+  })
+})
+
+ app.listen(3333, () => {
+   console.log('Back-end started!')
+ })
